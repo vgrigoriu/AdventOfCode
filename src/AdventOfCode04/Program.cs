bd369108a4663f1d8fb60defc8df6a5c1195d596
Victor Grigoriu <vgrigoriu@gmail.com>
2015-12-12 22:21:33 +0200

    Day 4

---
+++ src/AdventOfCode04/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode04
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var n = 1;
+            var md5 = MD5.Create();
+            while (true)
+            {
+                var input = $"ckczppom{n}";
+                var hash = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var hex = BitConverter.ToString(hash).Replace("-", "");
+                if (hex.StartsWith("000000"))
+                {
+                    Console.WriteLine(n);
+                    break;
+                }
+
+                n = n + 1;
+            }
+        }
+    }
+}
