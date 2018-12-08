96d7a82b1faf8417fa9bc59bb30e2b9328b1e685
Victor Grigoriu <vgrigoriu@gmail.com>
2018-12-08 14:12:39 +0200

    Day 1 Part 1

---
+++ src/2018/AdventOfCode01/ChronalCalibration/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChronalCalibration
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var lines = await File.ReadAllLinesAsync("input.txt");
+            var frequencies = from line in lines
+                              select ParseInt(line);
+
+            Console.WriteLine(frequencies.Sum());
+        }
+
+        private static int ParseInt(string line)
+        {
+            return int.Parse(line);
+        }
+    }
+}
