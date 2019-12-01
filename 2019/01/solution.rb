4a5d776ad05c30b839fb699a38c21070864b6304
Victor Grigoriu <grigoriu@adobe.com>
2019-12-01 09:10:37 +0200

    2019/01 part 1

--- 2019/01/solution.rb
+++ 2019/01/solution.rb
@@ -6,3 +6,14 @@ puts fuel_for_mass(12)
 puts fuel_for_mass(14)
 puts fuel_for_mass(1969)
 puts fuel_for_mass(100756)
+
+file = File.open("input")
+masses = file.readlines.map(&:chomp).map(&:to_i)
+
+puts masses
+
+fuels = masses.map { |mass| fuel_for_mass(mass) }
+
+puts fuels
+
+puts fuels.sum
