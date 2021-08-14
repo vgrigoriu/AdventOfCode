99440abbbf119a0f6793f039e72d98ecd018c7dc
Victor Grigoriu <grigoriu@adobe.com>
2021-08-14 07:30:44 +0300

    cleanup

--- 2015/14/cpp/include/reindeer.h
+++ 2015/14/cpp/include/reindeer.h
@@ -5,22 +5,22 @@
 #include <string>
 
 namespace advent_of_code {
-    class Reindeer {
-        // Donner can fly 9 km/s for 5 seconds, but then must rest for 38 seconds.
-        std::string name;
-        int speed;
-        int fly_time;
-        int rest_time;
-        int score{0};
+class Reindeer {
+  std::string name;
+  int speed;
+  int fly_time;
+  int rest_time;
+  int score{0};
 
 public:
-        int distance_after (int seconds) const;
-        void award_point() {++score;};
-        int get_score() const {return score;};
+  int distance_after(int seconds) const;
+  void award_point() { ++score; };
+  int get_score() const { return score; };
 
-        friend auto operator<<(std::ostream& os, const Reindeer& dt) ->std::ostream&;
-        friend auto operator>>(std::istream& os, Reindeer& dt) ->std::istream&;
-    };
+  friend auto operator<<(std::ostream &os, const Reindeer &dt)
+      -> std::ostream &;
+  friend auto operator>>(std::istream &os, Reindeer &dt) -> std::istream &;
+};
 } // namespace advent_of_code
 
 #endif
--- 2015/14/cpp/src/main.cpp
+++ 2015/14/cpp/src/main.cpp
@@ -9,57 +9,52 @@ using namespace std;
 using namespace advent_of_code;
 
 namespace {
-    void award_points_after(int seconds, vector<Reindeer>& reindeers) {
-        vector<int> distances{};
-        transform(reindeers.cbegin(),
-                reindeers.cend(),
-                back_inserter(distances),
-                [seconds](auto& r) {return r.distance_after(seconds);});
+void award_points_after(int seconds, vector<Reindeer> &reindeers) {
+  vector<int> distances{};
+  transform(reindeers.cbegin(), reindeers.cend(), back_inserter(distances),
+            [seconds](auto &r) { return r.distance_after(seconds); });
 
-        auto max_distance = max_element(distances.cbegin(), distances.cend());
+  auto max_distance = max_element(distances.cbegin(), distances.cend());
 
-        for_each(reindeers.begin(), reindeers.end(), [seconds, max_distance](auto& r) {
-            if (r.distance_after(seconds) == *max_distance) {
-                r.award_point();
-            }
-        });
-    }
+  for_each(reindeers.begin(), reindeers.end(),
+           [seconds, max_distance](auto &r) {
+             if (r.distance_after(seconds) == *max_distance) {
+               r.award_point();
+             }
+           });
 }
+} // namespace
 
 auto main(int argc, char **argv) -> int {
-    if (argc != 2) {
-        cout << "Usage: " << argv[0] << " path/to/input" << "\n";
-        return 1;
-    }
-    
-    ifstream in{argv[1]};
-    vector<Reindeer> reindeers{};
+  if (argc != 2) {
+    cout << "Usage: " << argv[0] << " path/to/input"
+         << "\n";
+    return 1;
+  }
 
-    copy(istream_iterator<Reindeer>{in},
-         istream_iterator<Reindeer>{},
-         back_inserter(reindeers));
+  ifstream in{argv[1]};
+  vector<Reindeer> reindeers{};
 
-    vector<int> distances{};
-    transform(reindeers.cbegin(),
-              reindeers.cend(),
-              back_inserter(distances),
-              [](auto& r) {return r.distance_after(2503);});
+  copy(istream_iterator<Reindeer>{in}, istream_iterator<Reindeer>{},
+       back_inserter(reindeers));
 
-    for (auto d: distances) {
-        cout << "Distance: " << d << "\n";
-    }
+  vector<int> distances{};
+  transform(reindeers.cbegin(), reindeers.cend(), back_inserter(distances),
+            [](auto &r) { return r.distance_after(2503); });
 
-    auto max_distance = max_element(distances.cbegin(), distances.cend());
+  auto max_distance = max_element(distances.cbegin(), distances.cend());
 
-    cout << "Maximum distance is " << *max_distance << "\n";
+  cout << "Maximum distance is " << *max_distance << "\n";
 
-    for (int s = 1; s <= 2503; ++s) {
-        award_points_after(s, reindeers);
-    }
+  for (int s = 1; s <= 2503; ++s) {
+    award_points_after(s, reindeers);
+  }
 
-    auto max_score_reindeer = max_element(reindeers.cbegin(), reindeers.cend(),
-        [](auto& r1, auto& r2) {return r1.get_score() < r2.get_score();});
+  auto max_score_reindeer =
+      max_element(reindeers.cbegin(), reindeers.cend(), [](auto &r1, auto &r2) {
+        return r1.get_score() < r2.get_score();
+      });
 
-    cout << "Maximum score is " << max_score_reindeer-> get_score() << "\n";
-    return 0;
+  cout << "Maximum score is " << max_score_reindeer->get_score() << "\n";
+  return 0;
 }
