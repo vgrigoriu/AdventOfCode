488d441ae90dc4ee4342a55d815d1fb90fe5a7ef
Victor Grigoriu <vgrigoriu@gmail.com>
2022-12-03 15:45:01 +0200

    refactor: make parsing a bit clearer

--- scala/aoc/src/main/scala/2015/AuntSue.scala
+++ scala/aoc/src/main/scala/2015/AuntSue.scala
@@ -12,38 +12,44 @@ object AuntSue2 extends Puzzle:
     val matchingAunts = aunts.filter(_.matches2(targetReading))
     matchingAunts(0).no
 
-val targetReading = Aunt(Map(
-        "children" -> 3,
-        "cats" -> 7,
-        "samoyeds" -> 2,
-        "pomeranians" -> 3,
-        "akitas" -> 0,
-        "vizslas" -> 0,
-        "goldfish" -> 5,
-        "trees" -> 3,
-        "cars" -> 2,
-        "perfumes" -> 1,
-    ), 0)
+val targetReading = Aunt(
+  Map(
+    "children" -> 3,
+    "cats" -> 7,
+    "samoyeds" -> 2,
+    "pomeranians" -> 3,
+    "akitas" -> 0,
+    "vizslas" -> 0,
+    "goldfish" -> 5,
+    "trees" -> 3,
+    "cars" -> 2,
+    "perfumes" -> 1
+  ),
+  0
+)
 case class Aunt(things: Map[String, Int], no: Int):
-    def matches(target: Aunt): Boolean =
-        things.keySet.forall(thing => things(thing) == target.things(thing))
-    
-    def matches2(target: Aunt): Boolean =
-        things.keySet.forall(thing => thing match {
-            // the cats and trees readings indicates that there are greater than that many
-            case "cats" | "trees" => things(thing) > target.things(thing)
-            // the pomeranians and goldfish readings indicate that there are fewer than that many
-            case "pomeranians" | "goldfish" => things(thing) < target.things(thing)
-            case _ => things(thing) == target.things(thing)
-        })
+  def matches(target: Aunt): Boolean =
+    things.keySet.forall(thing => things(thing) == target.things(thing))
+
+  def matches2(target: Aunt): Boolean =
+    things.keySet.forall(thing =>
+      thing match {
+        // the cats and trees readings indicates that there are greater than that many
+        // the pomeranians and goldfish readings indicate that there are fewer than that many
+        case "cats" | "trees"           => things(thing) > target.things(thing)
+        case "pomeranians" | "goldfish" => things(thing) < target.things(thing)
+        case _                          => things(thing) == target.things(thing)
+      }
+    )
 
 object Aunt:
-    def apply(input: String): Aunt =
-        // Sue 1: cars: 9, akitas: 3, goldfish: 0
-        val (auntNo, auntThings) = input.splitAt(input.indexOf(":"))
-        val things = auntThings.substring(2).split(", ")
-            .map(_.split(": "))
-            .map(parts => (parts(0), parts(1).toInt))
-            .toMap
-        
-        Aunt(things, auntNo.substring(4).toInt)
+  def apply(input: String): Aunt =
+    // Sue 1: cars: 9, akitas: 3, goldfish: 0
+    val Array(auntNo, auntThings) = input.substring(4).split(": ", 2)
+    val things = auntThings
+      .split(", ")
+      .map(_.split(": "))
+      .map(parts => (parts(0), parts(1).toInt))
+      .toMap
+
+    Aunt(things, auntNo.toInt)
