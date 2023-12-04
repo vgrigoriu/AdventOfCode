49c82fce26a8d852fa8003abc482339ac7d6f75b
Victor Grigoriu <grigoriu@adobe.com>
2023-12-04 17:58:40 +0200

    add template

---
+++ 2023/_template.py
@@ -0,0 +1,17 @@
+from input import read_input
+
+
+def solve_part_1(day: int):
+    puzzle_input = read_input(day)
+    return puzzle_input
+
+
+def solve_part_2(day: int):
+    puzzle_input = read_input(day)
+    return puzzle_input
+
+
+if __name__ == '__main__':
+    day = 42
+    print(solve_part_1(day))
+    print(solve_part_2(day))
