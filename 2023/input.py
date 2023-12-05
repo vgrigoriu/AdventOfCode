5e5ce40358169217b9cbc7ccb002f96274a58029
Victor Grigoriu <grigoriu@adobe.com>
2023-12-05 12:07:04 +0200

    add read_input_as_string

--- 2023/input.py
+++ 2023/input.py
@@ -6,3 +6,12 @@ def read_input(day: int, debug: bool = False) -> list[str]:
     with open(path) as f:
         lines = f.readlines()
         return [line.strip() for line in lines]
+
+
+def read_input_as_string(day: int, debug: bool = False) -> str:
+    # read all the lines
+    path = f'input/day{day:02}.txt'
+    if debug:
+        path = f'input/day{day:02}_debug.txt'
+    with open(path) as f:
+        return f.read()
