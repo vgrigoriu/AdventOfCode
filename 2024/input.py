aad7fd246d60884b4c690aaa0a9a6a7afda4a49f
Victor Grigoriu <grigoriu@adobe.com>
2024-12-05 07:24:50 +0200

    add -s flag to read small file

--- 2024/input.py
+++ 2024/input.py
@@ -1,4 +1,5 @@
 import inspect
+import sys
 from typing import Callable
 
 
@@ -7,7 +8,9 @@ def read_aoc_input[T](line_parser: Callable[[str], T]) -> list[T]:
     Reads the input file for the current day and returns a list of parsed lines.
     Assumes that the caller is in a file named dayX.py where X is the day number.
     """
+    use_small_file = len(sys.argv) == 2 and sys.argv[1] == "-s"
+    small_suffix = "small" if use_small_file else ""
     caller_filename = inspect.stack()[1].filename
     day = int(caller_filename.split("/")[-1].split(".")[0][3:])
-    with open(f"input/{day}.in") as f:
+    with open(f"input/{day}{small_suffix}.in") as f:
         return [line_parser(line) for line in f.readlines()]
