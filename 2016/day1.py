eb45639b9d9c2871e8a2fa1ab6b1b5951035ce15
Victor Grigoriu <grigoriu@adobe.com>
2024-12-07 20:13:32 +0200

    2016 day 1 part 2

--- 2016/day1.py
+++ 2016/day1.py
@@ -32,3 +32,35 @@ for side, blocks in input:
     walk(blocks)
 
 print(abs(lat) + abs(long))
+
+lat = 0
+long = 0
+dir = Direction()
+
+visited = [(0, 0)]
+def walk_memo(blocks: int) -> bool:
+    global lat, long
+    d_lat, d_long = 0, 0
+    if str(dir) == "N":
+        d_lat = 1
+    elif str(dir) == "S":
+        d_lat = -1
+    elif str(dir) == "E":
+        d_long = 1
+    elif str(dir) == "W":
+        d_long = -1
+    
+    for _ in range(blocks):
+        lat += d_lat
+        long += d_long
+        if (lat, long) in visited:
+            return True
+        visited.append((lat, long))
+
+for side, blocks in input:
+    dir.turn(side)
+    found = walk_memo(blocks)
+    if found:
+        break
+
+print(abs(lat) + abs(long))
