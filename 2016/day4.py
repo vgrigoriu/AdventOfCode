8aea9e6066d36be9e666058750e69489fa28a9ec
Victor Grigoriu <grigoriu@adobe.com>
2024-12-09 07:03:52 +0200

    refactor day 4

--- 2016/day4.py
+++ 2016/day4.py
@@ -11,6 +11,19 @@ class Room:
     sector_id: str
     checksum: str
 
+    def is_real(self) -> bool:
+        frequencies = [[chr(i), 0] for i in range(ord("a"), ord("z") + 1)]
+        for ch in self.name:
+            if ch == "-":
+                continue
+            frequencies[ord(ch) - ord("a")][1] += 1
+
+        # Stable sort, so alphabetical order is preserved in case of ties.
+        frequencies.sort(key=itemgetter(1), reverse=True)
+        common_letters = "".join(x[0] for x in frequencies[:5])
+
+        return common_letters == self.checksum
+
 
 def parse_room(line: str) -> Room:
     # bkwzkqsxq-tovvilokx-nozvyiwoxd-172[fstek]
@@ -30,20 +43,8 @@ def rotate_string(s: str, n: int) -> str:
 
 rooms = read_aoc_input(parse_room)
 
-part1 = 0
-real_rooms = []
-for room in rooms:
-    frequencies = [[chr(i), 0] for i in range(ord("a"), ord("z") + 1)]
-    for ch in room.name:
-        if ch == "-":
-            continue
-        frequencies[ord(ch) - ord("a")][1] += 1
-
-    frequencies.sort(key=itemgetter(1), reverse=True)
-    common_letters = "".join(x[0] for x in frequencies[:5])
-    if common_letters == room.checksum:
-        part1 += room.sector_id
-        real_rooms.append(room)
+real_rooms = [room for room in rooms if room.is_real()]
+part1 = sum(room.sector_id for room in real_rooms)
 
 print(part1)
 
