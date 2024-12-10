603f732a93d13657ee2c31d7b7f61843b3a22488
Victor Grigoriu <grigoriu@adobe.com>
2024-12-10 07:41:38 +0200

    2024 day 10 part 2

--- 2024/day10.py
+++ 2024/day10.py
@@ -1,43 +1,45 @@
 from input import read_aoc_input
 
-def find_trails(line: int, col: int) -> set[tuple[int, int]]:
-    if trail_heads[line][col] is not None:
-        return trail_heads[line][col]
+def find_trails(line: int, col: int) -> int:
+    if trails[line][col] != -1:
+        return trails[line][col]
 
     start = data[line][col]
     if start == "9":
         # we're done
-        trail_heads[line][col] = {(line, col)}
-        return trail_heads[line][col]
+        trails[line][col] = 1
+        return 1
 
-    trail_heads_here = set()
+    no_trails = 0
     # up
     if line > 0 and ord(start) + 1 == ord(data[line - 1][col]):
-        trail_heads_here = trail_heads_here.union(find_trails(line - 1, col))
+        no_trails += find_trails(line - 1, col)
     # down
     if line < len(data) - 1 and ord(start) + 1 == ord(data[line + 1][col]):
-        trail_heads_here = trail_heads_here.union(find_trails(line + 1, col))
+        no_trails += find_trails(line + 1, col)
     # left
     if col > 0 and ord(start) + 1 == ord(data[line][col - 1]):
-        trail_heads_here = trail_heads_here.union(find_trails(line, col - 1))
+        no_trails += find_trails(line, col - 1)
     # right
     if col < len(data[0]) - 1 and ord(start) + 1 == ord(data[line][col + 1]):
-        trail_heads_here = trail_heads_here.union(find_trails(line, col + 1))
+        no_trails += find_trails(line, col + 1)
     
-    trail_heads[line][col] = trail_heads_here
-    return trail_heads[line][col]
+    trails[line][col] = no_trails
+    return no_trails
 
 
 
-def score(line: int, col: int) -> int:
+def score(line: int, col: int):
     if data[line][col] != "0":
         return 0
-    return len(find_trails(line, col))
+    return find_trails(line, col)
 
+def print_trails():
+    pass
 
 data = read_aoc_input()
-trail_heads = [
-    [None for _ in line] for line in data
+trails = [
+    [-1 for _ in line] for line in data
 ]
 sum_of_scores = 0
 for line in range(len(data)):
@@ -45,6 +47,6 @@ for line in range(len(data)):
         s = score(line, col)
         if s > 0:
             sum_of_scores += score(line, col)
-
+            print_trails()
 
 print(sum_of_scores)
