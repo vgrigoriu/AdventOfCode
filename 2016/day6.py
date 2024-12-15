9d25d1c9438c9a3ec767b5b54fed90cfe7b62b1e
Victor Grigoriu <grigoriu@adobe.com>
2024-12-15 16:37:37 +0200

    ruff

--- 2016/day6.py
+++ 2016/day6.py
@@ -4,13 +4,9 @@ from input import read_aoc_input
 
 lines = read_aoc_input()
 
-columns = [
-    [line[col_no] for line in lines] for col_no in range(len(lines[0]))
-]
+columns = [[line[col_no] for line in lines] for col_no in range(len(lines[0]))]
 
-counters = [
-    Counter(col) for col in columns
-]
+counters = [Counter(col) for col in columns]
 
 decoded = [c.most_common(1)[0][0] for c in counters]
 print("".join(decoded))
