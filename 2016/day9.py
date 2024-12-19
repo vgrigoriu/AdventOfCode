8a886aebbf7573ca0a16dcd641bf37f8ed39426b
Victor Grigoriu <grigoriu@adobe.com>
2024-12-19 16:20:20 +0200

    2016 day 9 part 1 refactor

--- 2016/day9.py
+++ 2016/day9.py
@@ -2,25 +2,29 @@ import re
 
 from input import read_aoc_input
 
+
+def compute_decompressed_length(data: str) -> int:
+    position = 0
+    decompressed_length = 0
+    marker_pattern = re.compile(r"\((\d+)x(\d+)\)")
+    while position < len(data) - 1:
+        if m := marker_pattern.search(data, position):
+            # copy everything from current position until next marker
+            decompressed_length += (m.start() - position)
+            # take subsequent chars and repeat them
+            length, times = (int(g) for g in m.groups())
+            decompressed_length += (length * times)
+            # advance position
+            position = m.end() + length
+        else:
+            # no marker until the end of data, we're done
+            decompressed_length += (len(data) - position)
+            position = len(data)
+    
+    return decompressed_length
+
+
 compressed_data = read_aoc_input()
 
-print(compressed_data[:24])
-
-position = 0
-decompressed_length = 0
-marker_pattern = re.compile(r"\((\d+)x(\d+)\)")
-while position < len(compressed_data) - 1:
-    if m := marker_pattern.search(compressed_data, position):
-        # copy everything from current position until next marker
-        decompressed_length += (m.start() - position)
-        # take subsequent chars and repeat them
-        length, times = (int(g) for g in m.groups())
-        decompressed_length += (length * times)
-        # advance position
-        position = m.end() + length
-    else:
-        # no marker until the end of data, we're done
-        decompressed_length += (len(compressed_data) - position)
-        position = len(compressed_data)
-
-print(decompressed_length)
+part1 = compute_decompressed_length(compressed_data)
+print(part1)
