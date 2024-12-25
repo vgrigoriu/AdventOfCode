5d2166df56b5366bc8f8c83de77cf8ad96d63d9c
Victor Grigoriu <grigoriu@adobe.com>
2024-12-25 14:14:02 +0200

    2016 day 13

---
+++ 2016/day13grid.py
@@ -0,0 +1,109 @@
+import dataclasses
+import functools
+from collections.abc import Iterable
+from typing import Any
+
+from grid import Coord, Grid
+
+
+class Day13Grid(Grid):
+    def __init__(self, fav_num: int) -> None:
+        self._fav_num = fav_num
+        self._max_x = 0
+        self._max_y = 0
+
+    @functools.cache
+    def __getitem__(self, coord: Coord) -> Any:
+        x, y = dataclasses.astuple(coord)
+
+        # Did we go beyond?
+        self._max_x = max(x, self._max_x)
+        self._max_y = max(y, self._max_y)
+
+        value: int = x * x + 3 * x + 2 * x * y + y + y * y
+        value += self._fav_num
+        num_bits = value.bit_count()
+        if num_bits % 2 == 0:
+            return "."
+        else:
+            return "#"
+    
+    def __iter__(self):
+        for y in range(self._max_y + 1):
+            yield self._row_iter(y)
+    
+    def _row_iter(self, y: int) -> Iterable[str]:
+        for x in range(self._max_x + 1):
+            yield self[Coord(x, y)]
+
+    def is_visitable(self, coord: Coord) -> bool:
+        return coord.x >= 0 and coord.y >= 0 and self[coord] == "."
+
+
+def test_origin() -> None:
+    sut = Day13Grid(10)
+
+    result = sut[Coord(0, 0)]
+
+    assert result == "."
+
+def test_1_0() -> None:
+    sut = Day13Grid(10)
+
+    result = sut[Coord(1, 0)]
+
+    assert result == "#"
+
+def test_iterator_up_to_9_6() -> None:
+    sut = Day13Grid(10)
+
+    # extend gord up to (9, 6)
+    assert sut[Coord(9, 6)] == "#"
+
+    result = [
+        "".join(row) for row in sut
+    ]
+
+    assert result == [
+        ".#.####.##",
+        "..#..#...#",
+        "#....##...",
+        "###.#.###.",
+        ".##..#..#.",
+        "..##....#.",
+        "#...##.###"
+    ]
+
+def test_find_path_to_7_4() -> None:
+    sut = Day13Grid(10)
+
+    result = sut.shortest_path(Coord(1, 1), Coord(7, 4))
+
+    assert result == 11
+
+def test_part_1() -> None:
+    grid = Day13Grid(1352)
+    result = grid.shortest_path(Coord(1, 1), Coord(31, 39))
+
+    assert result == 90
+
+def test_visitable_max_steps_0() -> None:
+    sut = Day13Grid(10)
+    result = sut.how_many_visited(Coord(1, 1), 0)
+    assert result == 1
+
+def test_visitable_max_steps_1() -> None:
+    sut = Day13Grid(10)
+    result = sut.how_many_visited(Coord(1, 1), 1)
+    assert result == 3
+
+def test_visitable_max_steps_2() -> None:
+    sut = Day13Grid(10)
+    result = sut.how_many_visited(Coord(1, 1), 2)
+    assert result == 5
+
+def test_part_2() -> None:
+    grid = Day13Grid(1352)
+    result = grid.how_many_visited(Coord(1, 1), 50)
+
+    assert result == 135
---
+++ 2016/grid.py
@@ -0,0 +1,80 @@
+import dataclasses
+from abc import ABC, abstractmethod
+from collections.abc import Iterable
+from dataclasses import dataclass
+from queue import Queue
+from typing import Any
+
+
+@dataclass(frozen=True)
+class Coord:
+    x: int
+    y: int
+
+
+class Grid(ABC):
+    @abstractmethod
+    def __getitem__(self, coord: Coord) -> Any:
+        ...
+
+    @abstractmethod
+    def __iter__(self) -> Iterable[Iterable[Any]]:
+        """return an iterator that returns one iterator per row of the grid"""
+        ...
+
+    @abstractmethod
+    def is_visitable(self, coord: Coord):
+        ...
+
+    def shortest_path(self, start: Coord, target: Coord) -> int | None:
+        visited = set()
+        to_visit = Queue()
+        to_visit.put((start, 0))
+
+        while not to_visit.empty():
+            coord, distance = to_visit.get()
+            if coord in visited:
+                continue
+
+            visited.add(coord)
+
+            neighbors = [
+                dataclasses.replace(coord, x = coord.x - 1),
+                dataclasses.replace(coord, x = coord.x + 1),
+                dataclasses.replace(coord, y = coord.y - 1),
+                dataclasses.replace(coord, y = coord.y + 1),
+            ]
+
+            for neighbor in neighbors:
+                if neighbor == target:
+                    return distance + 1
+                if self.is_visitable(neighbor):
+                    to_visit.put((neighbor, distance + 1))
+
+        # no path found
+        return None
+
+    def how_many_visited(self, start: Coord, max_steps: int) -> int:
+        visited = {}
+        to_visit = Queue()
+        to_visit.put((start, 0))
+
+        while not to_visit.empty():
+            coord, distance = to_visit.get()
+            if distance == max_steps + 1:
+                return len(visited)
+            if coord in visited:
+                continue
+
+            visited[coord] = distance
+
+            neighbors = [
+                dataclasses.replace(coord, x = coord.x - 1),
+                dataclasses.replace(coord, x = coord.x + 1),
+                dataclasses.replace(coord, y = coord.y - 1),
+                dataclasses.replace(coord, y = coord.y + 1),
+            ]
+
+            for neighbor in neighbors:
+                if self.is_visitable(neighbor):
+                    to_visit.put((neighbor, distance + 1))
