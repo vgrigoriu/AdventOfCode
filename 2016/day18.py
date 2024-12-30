940281d5b9a4c79d2094b952c04ef6063291462d
Victor Grigoriu <grigoriu@adobe.com>
2024-12-30 08:39:49 +0200

    ruff

--- 2016/day18.py
+++ 2016/day18.py
@@ -7,23 +7,17 @@ room = [
     list("." + starting_row + ".")
 ] + [
     # plus extra 399_999 rows, to make 400_000
-    ["." for _ in range(len(starting_row) + 2)] for _ in range(399_999)
+    ["." for _ in range(len(starting_row) + 2)]
+    for _ in range(399_999)
 ]
 
-trap_patterns = [
-    "^^.",
-    ".^^",
-    "^..",
-    "..^"
-]
+trap_patterns = ["^^.", ".^^", "^..", "..^"]
 
 for i in range(1, 400_000):
     for j in range(1, len(starting_row) + 1):
-        if "".join(room[i - 1][j-1:j+2]) in trap_patterns:
+        if "".join(room[i - 1][j - 1 : j + 2]) in trap_patterns:
             room[i][j] = "^"
 
-safe_tiles = len(
-    [tile for line in room for tile in line if tile == "."]
-)
+safe_tiles = len([tile for line in room for tile in line if tile == "."])
 
 print(safe_tiles - 800_000)
